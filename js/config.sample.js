2607bee7c81fbf3338d6b8396ae8cd8a9e711cab
Will Spaetzel <will@spaetzel.com>
2013-10-18 12:39:15 -0400

    add missing comma to sample config

--- js/config.sample.js
+++ js/config.sample.js
@@ -3,7 +3,7 @@ define(['jquery', 'underscore', 'backbone'], function($, _, Backbone) {
 	var config = {
 		urls: [{
 			name: 'WatCamp Events',
-			url: "http://www.google.com/calendar/feeds/nlkc39jt4p0nbc4pk9pj7p5fh0%40group.calendar.google.com/public/basic?orderby=starttime&max-results=20&singleevents=true&sortorder=ascending&futureevents=true&ctz=America/New_York"
+			url: "http://www.google.com/calendar/feeds/nlkc39jt4p0nbc4pk9pj7p5fh0%40group.calendar.google.com/public/basic?orderby=starttime&max-results=20&singleevents=true&sortorder=ascending&futureevents=true&ctz=America/New_York",
 			color: "#00bd00"
 		}]
 	}
